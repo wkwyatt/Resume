d3fabc65045de5848c813955c75ec155d25f620a
wkwyatt <wkwyatt@gmail.com>
2015-10-16 14:03:30 -0400

    Initial Commit - Resume
    Professional resume with work related history, objectives, and skills.
    Skeleton doc basic layout.

---
+++ resume.php
@@ -0,0 +1,61 @@
+<? php   ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+
+		<div class="header">
+			<h1>Resume Header</h1>
+			<h3>Contact Info</h3>
+		</div>
+
+		<div class="body">
+
+			<div class="work-history">
+				<div class="work-history-item">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+				<div class="work-history-item">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>				
+				<div class="work-history-item">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+			</div> 
+
+			<div class="skills">
+				<ul>
+					<li>Ninja</li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="footer">
+		</div>
+
+	</div>
+</body>
+</html>
